e9a92418c65297c9add8e5a00680a73530bad288
stanleylexander <stanley.6ho@gmail.com>
2025-04-17 18:40:49 +0700

    Beautifull UI

--- js/script.js
+++ js/script.js
@@ -1,123 +1,68 @@
-/*!
-  * $script.js JS loader & dependency manager
-  * https://github.com/ded/script.js
-  * (c) Dustin Diaz 2014 | License MIT
-  */
+document.addEventListener("DOMContentLoaded", function() {
+  let userName = localStorage.getItem("userName");
 
-(function (name, definition) {
-  if (typeof module != 'undefined' && module.exports) module.exports = definition()
-  else if (typeof define == 'function' && define.amd) define(definition)
-  else this[name] = definition()
-})('$script', function () {
-  var doc = document
-    , head = doc.getElementsByTagName('head')[0]
-    , s = 'string'
-    , f = false
-    , push = 'push'
-    , readyState = 'readyState'
-    , onreadystatechange = 'onreadystatechange'
-    , list = {}
-    , ids = {}
-    , delay = {}
-    , scripts = {}
-    , scriptpath
-    , urlArgs
-
-  function every(ar, fn) {
-    for (var i = 0, j = ar.length; i < j; ++i) if (!fn(ar[i])) return f
-    return 1
-  }
-  function each(ar, fn) {
-    every(ar, function (el) {
-      return !fn(el)
-    })
-  }
-
-  function $script(paths, idOrDone, optDone) {
-    paths = paths[push] ? paths : [paths]
-    var idOrDoneIsDone = idOrDone && idOrDone.call
-      , done = idOrDoneIsDone ? idOrDone : optDone
-      , id = idOrDoneIsDone ? paths.join('') : idOrDone
-      , queue = paths.length
-    function loopFn(item) {
-      return item.call ? item() : list[item]
-    }
-    function callback() {
-      if (!--queue) {
-        list[id] = 1
-        done && done()
-        for (var dset in delay) {
-          every(dset.split('|'), loopFn) && !each(delay[dset], loopFn) && (delay[dset] = [])
-        }
+  if (!userName) {
+      userName = prompt("Masukkan nama Anda:");
+      if (userName) {
+          localStorage.setItem("userName", userName);
       }
-    }
-    setTimeout(function () {
-      each(paths, function loading(path, force) {
-        if (path === null) return callback()
-        
-        if (!force && !/^https?:\/\//.test(path) && scriptpath) {
-          path = (path.indexOf('.js') === -1) ? scriptpath + path + '.js' : scriptpath + path;
-        }
-        
-        if (scripts[path]) {
-          if (id) ids[id] = 1
-          return (scripts[path] == 2) ? callback() : setTimeout(function () { loading(path, true) }, 0)
-        }
-
-        scripts[path] = 1
-        if (id) ids[id] = 1
-        create(path, callback)
-      })
-    }, 0)
-    return $script
   }
 
-  function create(path, fn) {
-    var el = doc.createElement('script'), loaded
-    el.onload = el.onerror = el[onreadystatechange] = function () {
-      if ((el[readyState] && !(/^c|loade/.test(el[readyState]))) || loaded) return;
-      el.onload = el[onreadystatechange] = null
-      loaded = 1
-      scripts[path] = 2
-      fn()
-    }
-    el.async = 1
-    el.src = urlArgs ? path + (path.indexOf('?') === -1 ? '?' : '&') + urlArgs : path;
-    head.insertBefore(el, head.lastChild)
+  if (userName) {
+      document.getElementById("userName").textContent = userName;
   }
-
-  $script.get = create
-
-  $script.order = function (scripts, id, done) {
-    (function callback(s) {
-      s = scripts.shift()
-      !scripts.length ? $script(s, id, done) : $script(s, callback)
-    }())
-  }
-
-  $script.path = function (p) {
-    scriptpath = p
-  }
-  $script.urlArgs = function (str) {
-    urlArgs = str;
-  }
-  $script.ready = function (deps, ready, req) {
-    deps = deps[push] ? deps : [deps]
-    var missing = [];
-    !each(deps, function (dep) {
-      list[dep] || missing[push](dep);
-    }) && every(deps, function (dep) {return list[dep]}) ?
-      ready() : !function (key) {
-      delay[key] = delay[key] || []
-      delay[key][push](ready)
-      req && req(missing)
-    }(deps.join('|'))
-    return $script
-  }
-
-  $script.done = function (idOrDone) {
-    $script([null], idOrDone)
-  }
-
-  return $script
 });
+
+
+function toggleMenu() {
+  const navMenu = document.getElementById("navMenu");
+  navMenu.classList.toggle("active");
+}
+
+var slideIndex = 1;
+    showDivs(slideIndex)
+
+    function plusDivs(n){
+        showDivs((slideIndex += n));
+    }
+
+    function showDivs(n){
+        var i;
+        var x = document.getElementsByClassName("img-slideshow");
+        if(n > x.length){
+            slideIndex = 1;
+        }
+
+        else if(n < 1){
+            slideIndex = x.length;
+        }
+
+        for(i = 0; i < x.length; i++){
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+    setInterval(() => {
+        plusDivs(1);
+    }, 2000) //1 second
+
+
+    document.getElementById("formMessage").addEventListener("submit", function(e) {
+      e.preventDefault(); // mencegah form reload
+  
+      const name = document.getElementById("name").value;
+      const birthdate = document.getElementById("birthdate").value;
+      const gender = document.querySelector('input[name="gender"]:checked');
+      const message = document.getElementById("message").value;
+  
+      let genderValue = gender ? gender.value : "Belum dipilih";
+  
+      const result = 
+          `Nama: ${name}\n` +
+          `Tanggal Lahir: ${birthdate}\n` +
+          `Jenis Kelamin: ${genderValue}\n` +
+          `Pesan: ${message}`;
+  
+      document.getElementById("output").value = result;
+  });
